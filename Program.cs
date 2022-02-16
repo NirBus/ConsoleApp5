6d8f3c576ed721ac25d5fe02ee8c7f812f20700d
VenDiago <alizg@code.edu.az>
2022-02-16 08:47:09 +0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace ConsoleApp5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "Interesting opinions of Allahshukur";
+            {
+                ////for (int i = 0; i <= str.Length; i++)
+                ////{
+                ////    if (str.Length>0 || char.IsUpper([0]))
+                ////    {
+                ////        char[] 
+                ////    }
+                ////    {
+                ////        Console.WriteLine(i);
+                ////        //Console.WriteLine(str.ToUpper());
+                ////        //Console.WriteLine(str[i]);
+                ////    }
+                ////    //    }
+                ////    //char.IsUpper('A');
+                ////    //Console.WriteLine(str);
+
+
+
+            }
+            public static string Alik(String input)
+            {
+                for (int i = 0; i <= str.Length; i++)
+                {
+                    if (input.Length > 0)
+                }
+                char[] c = input.ToCharArray();
+                if (char.IsUpper(c[0]))
+                {
+                    c[0] = char.ToLower(c[0]);
+                }
+                else { c[0] = char.ToUpper(c[0]);
+                {
+                 return input;
+                }
+                }
+            }
+        }
+    }
+}
+            
+                
+                    
+
+               
